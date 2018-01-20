505bfd292e4dad2fdcee077625faad7dc731f1e0
zhangdongming1989 <806684620@qq.com>
2018-01-20 23:32:10 +0800

    start account

---
+++ src/router/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from '../App';
+
+export default (
+    <Route path="/">
+        <IndexRoute component={App} />
+    </Route>
+);
