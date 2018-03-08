93bcd4ffc54fb64ad7cb863fac667c631f543d79
zhangdongming <zhangdongming@zhihu.com>
2018-03-09 01:25:30 +0800

    proxy

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ yarn install
 yarn start
 ```
 
-关于API proxy
-方法1：可以charles 代理 api 到线上
-方法2：
-1.open -a Google\ Chrome --args --disable-web-security --user-data-dir ~/tmp/chrome/ 忽略跨域
-2.控制台输入 localStorage.setItem('realIp', true) 强制改写 api host 
+关于API proxy     
+连线上：localStorage.setItem('realIp', 1) 然后开 charles 代理 api 到线上        
+连本地 server  
+step1.open -a Google\ Chrome --args --disable-web-security --user-data-dir ~/tmp/chrome/ 忽略跨域
+step2.控制台输入 localStorage.setItem('realIp', 0)（或者把这条ls 清掉） 用线下 API 127.0.0.1:5000
  
--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,10 @@
-const useRealIp = localStorage.getItem('realIp');
-export const API_ROOT =
-    `${['localhost', '127.0.0.1'].includes(location.hostname) && (useRealIp === 'true')
-        ? 'http://47.93.202.85' : '' }/bookStore/webapi`;
+const useRealIp = Boolean(localStorage.getItem('realIp'));
+export const API_ROOT = (() => {
+    if (['localhost', '127.0.0.1'].includes(location.hostname)) {
+        return useRealIp ? '/bookStore/webapi' : 'http://127.0.0.1:5000/bookStore/webapi';
+    }
+    return '/bookStore/webapi';
+})();
 export const MESSAGE_DURATION = 3000;
 export const DATEPICKER_START_DATE = '2018-01-01';
 export const DATE_FORMAT = 'YYYY-MM-DD';
